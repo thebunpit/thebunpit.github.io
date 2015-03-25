0576bc32bffbe38a42347f99570992eb2a67f7fb
Betty Huang <bettyhuang@nat-oitwireless-inside-vapornet100-c-28468.princeton.edu>
2015-03-24 21:13:40 -0400

    template

---
+++ scrape.py
@@ -0,0 +1,12 @@
+import bs4
+import urllib2
+
+url = ''
+page = urllib2.urlopen(url).read()
+soup = BeautifulSoup(page)
+
+soup.find("title").getText()
+tagg.getText()
+soup.find(All("h1")
+sou.find("span", {"class":"mw-headline"})
+
